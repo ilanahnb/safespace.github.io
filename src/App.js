ff2d5e5e32489c3bd0d6b23972d520a0eb7cd9c5
Ilana Boeira <ilanaboeira1@bitbucket.org>
2025-08-07 16:02:31 +1000

    Update homepage URL in package.json and remove basename from Router in App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <Router basename="/safespace.github.io">
+      <Router>
         <AppNavbar />
         <main>
           <Routes>
